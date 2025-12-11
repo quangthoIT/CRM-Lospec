3891d9105e8668bf51edbc49bf5b81544b9d27f0
quangthoIT <quangthoIT04@gmail.com>
2025-12-11 22:52:38 +0700

    Chuẩn bị deloy neon

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,6 +7,10 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 pool.connect((err, client, release) => {
